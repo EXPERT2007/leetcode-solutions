586bf26a99106c51b137849f4724cbafc8020600
Halil İbrahim Kutmur <halilkutmur@gmail.com>
2025-01-29 11:56:11 +0300

    Solution Added
    412. Fizz Buzz

---
+++ easy/easy/S_412.h
@@ -0,0 +1,12 @@
+#ifndef S_412_H
+#define S_412_H
+
+#include <vector>
+#include <string>
+
+class Solution {
+public:
+    std::vector<std::string> fizzBuzz(int n);
+};
+
+#endif // S_412_H
--- easy/easy/main.cpp
+++ easy/easy/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include<vector>
 #include<cmath>
+#include <string>
+#include <algorithm>
 using namespace std;
 #pragma region problems
 #include "S_2235.h"
 #include "S_605.h"
 #include "S_1672.h"
+#include "S_412"
 #pragma endregion
 
 
