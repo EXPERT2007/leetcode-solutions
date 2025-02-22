270a395f34da3422ddb072bb10e1483fcb460131
Halil İbrahim Kutmur <halilkutmur@gmail.com>
2025-02-22 21:27:47 +0300

    Minor Change on S_1512.cpp

--- easy/easy/S_1512.cpp
+++ easy/easy/S_1512.cpp
@@ -1,13 +1,13 @@
 #include "S_1512.h"
 
-int Solution::numIdenticalPairs(std::vector<int>& nums) {
-    int counter = 0;
-    for (size_t i = 0; i < nums.size(); i++) {
-        for (size_t j = i + 1; j < nums.size(); j++) { // Optimize: start j from i+1
-            if (nums[i] == nums[j]) {
-                counter++;
-            }
-        }
-    }
-    return counter;
-}
+//int Solution::numIdenticalPairs(std::vector<int>& nums) {
+//    int counter = 0;
+//    for (size_t i = 0; i < nums.size(); i++) {
+//        for (size_t j = i + 1; j < nums.size(); j++) { // Optimize: start j from i+1
+//            if (nums[i] == nums[j]) {
+//                counter++;
+//            }
+//        }
+//    }
+//    return counter;
+//}
